ae69bf11f8ff0dfb682cab0c8ab77e0c27504ae1
galdevops <galdevops@gmail.com>
2025-10-30 16:10:34 +0200

    custom flyto  mobile3

--- app/page.js
+++ app/page.js
@@ -30,7 +30,34 @@ export default function Home() {
         countries={countriesData.features}
         onSelectCountry={(country) => {
           const countryProps = country.properties;
-          handleSearchCountryFlyTo(countryProps);
+          
+          const lat = parseFloat(countryProps.label_y);
+          const lng = parseFloat(countryProps.label_x);
+          const isMobile = window.matchMedia("(max-width: 767px)").matches;
+          let offsetY = 0;
+
+          if (isMobile) {
+            setTimeout(() => {
+              const panel = document.getElementById("country-info-panel");
+              offsetY = panel ? panel.offsetHeight / 2 : 0;
+
+              worldMapRef.current.flyTo({
+                center: [lng, lat],
+                zoom: 4,
+                essential: true,
+                speed: 0.8,
+                offset: [0, -offsetY],
+              });
+            }, 150);
+          }
+          else {
+            worldMapRef.current.flyTo({
+            center: [lng, lat],
+            zoom: 4,
+            essential: true,
+            speed: 0.8,
+            offset: [0, -offsetY]
+          });}
 
           setSelectedCountry({
             name: countryProps.name,
